9177bf814e0eadf3fe07aed84fcc12926a0f2f81
Nithin <nithinsubbiah@gmail.com>
2019-10-18 22:01:36 -0400

    ransac -not finished

--- vanishing_point_detection/detect_vp.py
+++ vanishing_point_detection/detect_vp.py
@@ -4,6 +4,11 @@ import cv2
 import numpy as np 
 
 def cluster_lines(lines, bin_size = 10):
+	'''
+	Input: 1. Lines that needs to be clustered
+		   2. Size of the bin to be used in histogram
+	Output: List of list of clustered lines 
+	'''
 
 	# To avoid the problem of dividing by zero, 1e-5 is added to the denominator
 	slope = (lines[:,3]-lines[:,2])/((lines[:,1]-lines[:,0])+1e-5)
@@ -28,7 +33,10 @@ def show_clusters(image, clusters):
 	'''
 	Input: 1. Image on which the cluster of lines is to be plotted
 		   2. A list of list of numpy arrays belonging to each cluster
-	Output: Shows clusters on the image
+	
+	Output: None
+
+	Description: Displays clusters on the image
 	'''
 
 	for cluster in clusters:
@@ -40,6 +48,34 @@ def show_clusters(image, clusters):
 
 	cv2.imshow('img', image)
 	cv2.waitKey(0)
+
+def RANSAC(cluster, iterations = 1000, threshold = 0.1):
+	
+	max_inliers = 0
+
+	for i in range(iterations):
+		
+		num_inliers = 0
+		data_1, data_2 = cluster[np.random.randint(len(cluster),size=2)]
+		line_1 = np.cross([data_1[0],data_1[2],1],[data_1[1],data_1[3],1])
+		line_2 = np.cross([data_2[0],data_2[2],1],[data_2[1],data_2[3],1])
+		VP = np.cross(line_1, line_2)
+
+		for data in cluster:
+			line = np.cross([data[0],data[2],1],[data[1],data[3],1])
+			# Error metric: distance of the VP from each line
+			distance = np.dot(line, VP)/np.sqrt(line[0]**2 + line[1]**2)
+			
+			if distance > threshold:
+				num_inliers += 1
+		
+		if num_inliers > max_inliers:
+
+
+def refine_clusters(clusters):
+
+	for cluster in clusters:
+		RANSAC(cluster)
 
 def main():
 	img_folder = '/home/nithin/Desktop/Geometry Vision/Assignments/HW2/images/input'
@@ -59,19 +95,9 @@ def main():
 	clusters = cluster_lines(lines)
 
 	# show_clusters(img, clusters)
-	
-	
+	refine_clusters(clusters)
 
-	'''
-
-	for line in lines:
-		x1, x2, y1, y2, _, _ = line 
-		cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), [0, 255, 0], 2) 
 	
-
-	cv2.imshow(img_name, img)
-	cv2.waitKey(0)
-	'''
 
 
 if __name__ == '__main__':
